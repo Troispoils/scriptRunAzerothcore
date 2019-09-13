4c14c46ee179d9b044465ac58a3d88db132ba02e
Troispoils <troispoils@live.fr>
2019-09-13 15:00:51 +0200

    Stopserv
    +Add fonction for kill server

--- scriptAutoRun.sh
+++ scriptAutoRun.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 DIRINSTALL='/home/troispoils/Documents/source/azerothserv'
-
+DIRSOURCE='/home/troispoils/Documents/source/azerothcore'
+DIRPARENT='/home/troispoils/Documents/source'
 
 
 recup_source () {
-        cd "/home/troispoils/Documents/source"
+        cd $DIRPARENT
         if [ $1 = "wipe" ] ;then
                 echo "Supression de l'ancienne source :";
                 sudo rm -r azerothcore
@@ -18,7 +19,7 @@ recup_source () {
 }
 
 maj_source () {
-        cd "/home/troispoils/Documents/source/azerothcore/"
+        cd $DIRSOURCE
         git pull
         compiling
 }
@@ -26,7 +27,7 @@ maj_source () {
 compiling () {
         echo "Comencer la compilation?";
         read
-        cd "/home/troispoils/Documents/source/azerothcore"
+        cd $DIRSOURCE
         rm -r build
         mkdir build
         cd "build"
@@ -35,7 +36,36 @@ compiling () {
         make install
 }
 
-if [ -d "/home/troispoils/Documents/source/azerothcore" ] ;then
+startup () {
+	echo "Starting server...";
+	cd $DIRINSTALL
+	screen -dmS login ./auhtserver
+	screen -dmS world ./worldserver
+}
+
+stopserv () {
+	idw=$(pidof worldserver)
+	idl=$(pidof authserver)
+	
+	echo "Verification que le server et pas lancer...";
+	
+	if [ $idw != "" ] ;then
+		echo "World present, kill du world.";
+		kill $idw
+	else
+		echo "World non present";
+	fi
+	if [ $idl != "" ] ;then
+		echo "Auth present, kill du auth.";
+		kill $idl
+	else
+		echo "Auth non present";
+	fi
+}
+
+stopserv
+
+if [ -d $DIRSOURCE ] ;then
         echo "Le dossier existe deja.";
         echo "1 - Update Source";
         echo "2 - Wipe and News";
@@ -49,10 +79,11 @@ else
         echo "Le dossier existe pas !";
         echo "Creation de la source et du dossier.";
         read
-        cd "/home/troispoils/Documents/source"
+        cd $DIRPARENT
         recup_source nowipe
 fi
 
 ## commande launch ##
 # screen -dmS login ./auhtserver
 # screen -dmS world ./worldserver
+# pidof worldserver ## verifier si le server est en ligne
